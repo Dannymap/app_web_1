01a068e8282fb8e37220c01a4523e45b30e26343
Dannymap <dannyavilan@gmail.com>
2023-12-29 19:32:01 +0100

    Initial Commot

---
+++ web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("Tareas Pendientes")
+st.subheader("Esta es mi App para hacer cosas.")
+st.write("Esta App es para aumentar tu productividad.")
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Agregar nuevas tareas...",
+              on_change=add_todo, key='new_todo')
