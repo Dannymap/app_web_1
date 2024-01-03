3c2a8d27f7d46dfa52cfb61ed7f11250806c5947
Dannymap <dannyavilan@gmail.com>
2024-01-03 22:06:29 +0100

    Archivos de configuración new

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template 
+app = Flask(__name__)
+# rutas
+@app.route('/')
+def raiz():
+    titulo = "pagina inicio"
+    return render_template('web.py', titulo=titulo)
+
+# ruta para nosotros
+"""
+@app.route('/nosotros')
+def nosotros():
+    titulo = "nosotros"
+    return render_template('nosotros.html', titulo=titulo)
+"""
+# bloque de prueba
+if __name__ == "__main__":
+    app.run(debug=True)
